5f020a34b51cc4526eadb87a698ffd12fb9d8b6e
Adam Jonas <adamjonas25@gmail.com>
2025-01-03 21:02:44 +0000

    Set up ft plugins

--- after/plugin/markdown-preview.lua
+++ after/plugin/markdown-preview.lua
@@ -1,2 +1,2 @@
--- vim.keymap.set('n', '<C-s>', vim.MarkdownPreview)
--- vim.keymap.set('n', '<C-p>', vim.MarkdownPreviewToggle)
+vim.keymap.set('n', '<leader>mp', vim.cmd.MarkdownPreviewToggle)
+vim.keymap.set('n', '<C-m>', vim.cmd.MarkdownPreview)
