196608eec440c573fec86452efc39cdacc9a0544
Adam Jonas <adamjonas25@gmail.com>
2025-01-03 22:53:34 +0000

    Add lualine

--- lua/aj8gh/packer.lua
+++ lua/aj8gh/packer.lua
@@ -43,6 +43,11 @@ return require('packer').startup(function(use)
     end,
   })
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
   use('petertriho/nvim-scrollbar')
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
