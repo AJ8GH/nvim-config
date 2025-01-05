599228551cb85428d753aceacc75547f64cbe835
Adam Jonas <adamjonas25@gmail.com>
2025-01-05 10:47:54 +0000

    Update toggleterm mappings

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,11 +1,11 @@
 require('toggleterm').setup()
 
 function _G.set_terminal_keymaps()
-local opts = {buffer = 0}
-vim.keymap.set('t', '<C-`>', [[<C-\><C-n><:ToggleTerm><CR>]], opts)
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<C-a>', [[<C-\><C-n><:ToggleTerm><CR>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-vim.keymap.set('n', '<C-`>', '<:ToggleTerm><CR>')
+vim.keymap.set('n', '<C-a>', '<:ToggleTerm><CR>')
--- lua/aj8gh/packer.lua
+++ lua/aj8gh/packer.lua
@@ -72,62 +72,62 @@ return require('packer').startup(function(use)
   --   branch= 'master',
   -- }
 
-    -- Test
-    use({
-      'nvim-neotest/neotest',
-      requires = {
-        'nvim-neotest/neotest-go',
-        'nvim-neotest/nvim-nio',
-        'nvim-lua/plenary.nvim',
-        'antoinemadec/FixCursorHold.nvim',
-        'nvim-treesitter/nvim-treesitter'
-      },
-    })
+  -- Test
+  use({
+    'nvim-neotest/neotest',
+    requires = {
+      'nvim-neotest/neotest-go',
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter'
+    },
+  })
 
-    -- Debugger
-    use {
-      'mfussenegger/nvim-dap',
-      requires = {
-        'leoluz/nvim-dap-go',
-        'rcarriga/nvim-dap-ui',
-        'theHamsta/nvim-dap-virtual-text',
-        'nvim-neotest/nvim-nio',
-        'williamboman/mason.nvim',
-      },
-    }
-    use 'mfussenegger/nvim-dap-python'
-    use {
+  -- Debugger
+  use {
+    'mfussenegger/nvim-dap',
+    requires = {
+      'leoluz/nvim-dap-go',
       'rcarriga/nvim-dap-ui',
-      requires = {
-        'mfussenegger/nvim-dap',
-        "nvim-neotest/nvim-nio"
-      }
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+    },
+  }
+  use 'mfussenegger/nvim-dap-python'
+  use {
+    'rcarriga/nvim-dap-ui',
+    requires = {
+      'mfussenegger/nvim-dap',
+      "nvim-neotest/nvim-nio"
     }
-    use 'theHamsta/nvim-dap-virtual-text'
-    use {'Weissle/persistent-breakpoints.nvim'}
-    use 'leoluz/nvim-dap-go'
+  }
+  use 'theHamsta/nvim-dap-virtual-text'
+  use {'Weissle/persistent-breakpoints.nvim'}
+  use 'leoluz/nvim-dap-go'
 
-    -- LSP
-    use {
-      'VonHeikemen/lsp-zero.nvim',
-      requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-      }
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
     }
+  }
 
-  end)
+end)
