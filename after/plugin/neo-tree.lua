1150917b1a30f55acfad2763a948f1db009b41c1
Adam Jonas <adamjonas25@gmail.com>
2025-01-05 21:46:25 +0000

    Fix neotree

--- after/plugin/neo-tree.lua
+++ after/plugin/neo-tree.lua
@@ -11,12 +11,12 @@
 --     }
 --   }
 -- })
---
---
--- -- optionally enable 24-bit colour
--- vim.opt.termguicolors = true
---
--- -- Toggle tree
--- vim.keymap.set("n", "<leader>x", "<cmd>Neotree toggle<CR>", { silent = true })
--- vim.keymap.set("n", "<leader>t", "<cmd>Neotree focus<CR>", { silent = true })
--- vim.keymap.set("n", "<leader>r", "<cmd>Neotree reveal<CR>", { silent = true })
+
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- Toggle tree
+vim.keymap.set("n", "<leader>x", "<cmd>Neotree toggle<CR>", { silent = true })
+vim.keymap.set("n", "<leader>t", "<cmd>Neotree focus<CR>", { silent = true })
+vim.keymap.set("n", "<leader>r", "<cmd>Neotree reveal<CR>", { silent = true })
--- lua/aj8gh/packer.lua
+++ lua/aj8gh/packer.lua
@@ -34,16 +34,16 @@ return require('packer').startup(function(use)
     branch = 'harpoon2',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  -- use {
-  --   "nvim-neo-tree/neo-tree.nvim",
-  --   branch = "v3.x",
-  --   requires = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  --     "MunifTanjim/nui.nvim",
-  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  --   }
-  -- }
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
   use 'karb94/neoscroll.nvim'
   use {'kevinhwang91/nvim-bqf', ft = 'qf'}
   use {
